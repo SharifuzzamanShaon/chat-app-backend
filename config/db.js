c804cffad6ee04fba4f76f443727631dd5e8023e
sharifuzzamanshaon <shasan2200@gmail.com>
2023-07-25 20:32:29 +0600

    add mogodb uri2

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB connected");
     } catch (error) {
         console.log(error);
